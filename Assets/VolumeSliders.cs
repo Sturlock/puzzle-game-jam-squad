a9e61920ff7a5848b439daff684dc152b69efbcb
rutkin.ryan@gmail.com <rutkin.ryan@gmail.com>
2021-07-17 11:50:42 -0500

    Added audio mixer
    Added the audio mixer and tied the volume slider to the master control.

---
+++ Assets/VolumeSliders.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+[RequireComponent(typeof (Slider))]
+public class VolumeSliders : MonoBehaviour {
+
+    Slider slider {
+        get { return GetComponent<Slider>(); }
+    }
+
+    public AudioMixer mixer;
+    public string volumeName;
+
+    public void UpdateValueOnChange(float value) {
+        mixer.SetFloat(volumeName, value);
+    }
+}
